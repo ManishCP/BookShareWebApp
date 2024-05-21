2ee5fb80659ebc79616d0eb0a5b4ff44abad4658
ManishCP <manishcp77@gmail.com>
2024-05-21 09:36:58 -0400

    Add project files.

---
+++ BookShare/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using BookShare.Models;
+
+namespace BookShare.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Book/Random
+        public ActionResult Random()
+        {
+            var Book = new Book() { Name = "Merchent of Venice!" };
+            return View(Book);
+        }
+    }
+}
